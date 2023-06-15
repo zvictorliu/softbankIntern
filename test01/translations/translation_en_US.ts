e5eae730ec9c525b9cffd0dd08f429a06785b288
zvictorliu <zvictorliu@outlook.com>
2023-06-15 17:07:59 +0800

    v1 checked

---
+++ test01/translations/translation_en_US.ts
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello Human</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Human</translation>
+        </message>
+        <message>
+            <source>你好</source>
+            <comment>Text</comment>
+            <translation type="obsolete">你好</translation>
+        </message>
+        <message>
+            <source>hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">hello</translation>
+        </message>
+        <message>
+            <source>中文</source>
+            <comment>Text</comment>
+            <translation type="obsolete">中文</translation>
+        </message>
+    </context>
+    <context>
+        <name>hello/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>main/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>AI</source>
+            <comment>Text</comment>
+            <translation type="obsolete">AI</translation>
+        </message>
+        <message>
+            <source>A ai </source>
+            <comment>Text</comment>
+            <translation type="obsolete">A ai </translation>
+        </message>
+        <message>
+            <source>A I</source>
+            <comment>Text</comment>
+            <translation type="obsolete">A I</translation>
+        </message>
+        <message>
+            <source>诶爱的发展</source>
+            <comment>Text</comment>
+            <translation type="obsolete">诶爱的发展</translation>
+        </message>
+        <message>
+            <source>欸埃的发展</source>
+            <comment>Text</comment>
+            <translation type="obsolete">欸埃的发展</translation>
+        </message>
+        <message>
+            <source>诶埃的发展</source>
+            <comment>Text</comment>
+            <translation type="obsolete">诶埃的发展</translation>
+        </message>
+        <message>
+            <source>A埃的发展</source>
+            <comment>Text</comment>
+            <translation type="obsolete">A埃的发展</translation>
+        </message>
+        <message>
+            <source>创造力和设计能力是诶埃无法完全替代的重要人类特质</source>
+            <comment>Text</comment>
+            <translation type="obsolete">创造力和设计能力是诶埃无法完全替代的重要人类特质</translation>
+        </message>
+        <message>
+            <source>虽然 A I可能在某些领域取代了一部分工作</source>
+            <comment>Text</comment>
+            <translation type="obsolete">虽然 A I可能在某些领域取代了一部分工作</translation>
+        </message>
+        <message>
+            <source>虽然诶埃可能在某些领域取代了一部分工作</source>
+            <comment>Text</comment>
+            <translation type="obsolete">虽然诶埃可能在某些领域取代了一部分工作</translation>
+        </message>
+        <message>
+            <source>虽然A I 可能在某些领域取代了一部分工作</source>
+            <comment>Text</comment>
+            <translation type="obsolete">虽然A I 可能在某些领域取代了一部分工作</translation>
+        </message>
+        <message>
+            <source>是的，
+
+高考只是通向成功的第一步，
+
+高考后的专业选择同样至关重要，
+
+一个好的专业不仅可以帮助学生提升学习动力，
+
+更能帮助未来的就业与长期发展</source>
+            <comment>Text</comment>
+            <translation type="obsolete">是的，
+
+高考只是通向成功的第一步，
+
+高考后的专业选择同样至关重要，
+
+一个好的专业不仅可以帮助学生提升学习动力，
+
+更能帮助未来的就业与长期发展</translation>
+        </message>
+    </context>
+    <context>
+        <name>main/behavior.xar:/Say (1)</name>
+        <message>
+            <source>虽然</source>
+            <comment>Text</comment>
+            <translation type="obsolete">虽然</translation>
+        </message>
+    </context>
+    <context>
+        <name>william/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>我，机器人</source>
+            <comment>Text</comment>
+            <translation type="obsolete">我，机器人</translation>
+        </message>
+        <message>
+            <source>I am a robot</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am a robot</translation>
+        </message>
+        <message>
+            <source>这是一个长文本</source>
+            <comment>Text</comment>
+            <translation type="obsolete">这是一个长文本</translation>
+        </message>
+        <message>
+            <source>你好，我是Pepper机器人</source>
+            <comment>Text</comment>
+            <translation type="obsolete">你好，我是Pepper机器人</translation>
+        </message>
+        <message>
+            <source>你好</source>
+            <comment>Text</comment>
+            <translation type="obsolete">你好</translation>
+        </message>
+    </context>
+    <context>
+        <name>william/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>这是一个更长的文本</source>
+            <comment>Text</comment>
+            <translation type="obsolete">这是一个更长的文本</translation>
+        </message>
+    </context>
+</TS>
---
+++ test01/translations/translation_zh_CN.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>main/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">你好</translation>
+        </message>
+    </context>
+</TS>
